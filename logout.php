12a33df60740cc1b6c6b1cfff1168628ac2c0ac7
Jeff <jeffersonpereira138@gmail.com>
2024-08-03 20:54:22 -0300

    exit()
    Incremento de exit() depois de usar header().

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
-<?php 
-
-if(!isset($_SESSION)){
+<?php
+if (!isset($_SESSION)) {
     session_start();
 }
 
 session_destroy();
 
-header("Location: index.php")
+header("Location: index.php");
+exit();
 ?>
