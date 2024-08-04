9bbda0a5d7a374bf4041a46b75af1b64e1bdf4ab
Jeff <jeffersonpereira138@gmail.com>
2024-08-03 21:02:59 -0300

    verificação
    Faz uma verificação do e-mail e da senha (password_verify). Agora a consulta SQL passa por uma preparação.

--- index.php
+++ index.php
@@ -1,40 +1,34 @@
-<?php 
+<?php
 include('conexao.php');
+session_start();
 
-if (isset($_POST['email']) || isset($_POST['senha'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $mysqli->real_escape_string(trim($_POST['email']));
+    $senha = $mysqli->real_escape_string(trim($_POST['senha']));
 
-    if(strlen($_POST['email'])==0){
-        echo "Prenencha seu e-mail";
-    } else if (strlen($_POST['senha'])==0) {
-        echo "Preencha sua senha";
-    } else{
-        #CONSULTA SQL
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli ->real_escape_string($_POST['senha']);
+    if (strlen($email) == 0) {
+        echo 'Preencha seu e-mail';
+    } else if (strlen($senha) == 0) {
+        echo 'Preencha sua senha';
+    } else {
+        $stmt = $mysqli->prepare("SELECT * FROM usuarios WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $usuario = $result->fetch_assoc();
 
-        $sql_code= "SELECT * FROM usuarios WHERE email = '$email' AND senha ='$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do SQL:" . $mysqli->error);
-
-        #VERIFICA SE DADOS ESTÃO NO BANCO DE DADOS
-        $quantidade = $sql_query->num_rows;
-
-        if($quantidade == 1){
-            $usuario = $sql_query->fetch_assoc();
-
-            if(!isset($_SESSION)){
-                session_start();
-            }
+        if ($usuario && password_verify($senha, $usuario['senha'])) {
             $_SESSION['id'] = $usuario['id'];
             $_SESSION['nome'] = $usuario['nome'];
-
             header("Location: painel.php");
-
-        }else{
-            echo "E-mail ou senha incorretos";
+            exit();
+        } else {
+            echo 'E-mail ou senha incorretos';
         }
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -45,13 +39,12 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
         <h1>Acesse sua conta</h1>
         <input type="text" name="email" id="email" placeholder="Seu e-mail">
         <input type="password" name="senha" id="senha" placeholder="Senha">
         <input type="submit" value="Entrar">
         <a href="cadastro.php">Faça seu <strong>CADASTRO!</strong></a>
-   
     </form>
 </body>
 </html>
